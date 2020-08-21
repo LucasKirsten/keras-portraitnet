79b8a6237b84d1b3f6774bc232a7a769c5497378
LucasKirsten <lucasnkir@gmail.com>
2020-08-21 18:56:39 -0300

    complete implementation (not tested yet)

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 A Keras implementation of the Portrait Net architecture. Original paper available at: https://www.yongliangyang.net/docs/mobilePotrait_c&g19.pdf
 
-Work in progress... but it should work :D
+## Pre-requisites
+- ```Python >= 3.6 ```
+- ```Tensorflow 2.x```
+- ```Imgaug```
+- ```OpenCV```
+- ```Jupyter Notebook```
+
+## Expected dataset
+It is expected that the dataset is contained in one or more folders. Each folder must contain the input image with suffix ```_image``` and all the segmentation masks with suffix ```_mask_<nr of class>``` (starts at 0). An example of folder struture can be:
+```
+- Dataset 1
+-- img1_image.jpg
+-- img1_mask_0.jpg
+-- img1_mask_2.jpg
+```
+This shows an example where the ```img1``` has 2 segmentation masks with ids 0 and 2. The segmentation for the id class 1, as it is not provided, will be considere as a zero matrix.
+
+**Important**: it is not expected the contour images, since the data generator script grab them using the find contours algorithm from OpenCV in the segmentation masks.
+
+## Usage
+You can start using this project with the provided jupyter notebook containing all aditional informations.
+
+## Contributions
+Contributions are allways welcome to improve this repository :D
--- model/data.py
+++ model/data.py
@@ -1,5 +1,3 @@
-''' This script hold the class to handle the dataset (pre-processing, data loader, data visualization etc). You can modify the methods of this class as you wish as long as you keep their functionalities purposes. '''
-
 import sys
 sys.path.append('./')
 from utils import *
@@ -16,17 +14,30 @@ from glob import glob
 
 class DataLoader(object):
     ''' Data loader class examplified when we have the data in memory '''
-    def __init__(self, folders_train, folders_val):
-        ''' All paths to the images to train and validate '''
+    def __init__(self, folders_train, folders_val, num_classes):
+        '''
+        All paths to the images to train and validate.
+        
+        Inputs
+        ---------
+        folders_train : list
+            A list of folders where are the dataset to train.
+        folders_val : list
+            A list of folders where are the dataset to validation.
+        num_classes : int
+            Number of classes
+        '''
+        
+        self.num_classes = num_classes
         
         # map paths and split dataset
         self.path_train = []; self.path_test = []
 
         for path in folders_train:
-            self.path_train.extend(glob(path+'/*_image.jpg'))
+            self.path_train.extend(glob(os.path.join(path,'*_image.jpg')))
 
         for path in folders_val:
-            self.path_test.extend(glob(path+'/*_image.jpg'))
+            self.path_test.extend(glob(os.path.join(path,'*_image.jpg')))
 
         print(f'Total train: {len(self.path_train)}, Total val: {len(self.path_test)}')
         
@@ -45,7 +56,18 @@ class DataLoader(object):
                 ])
         
     def data_size(self, data):
-        ''' Return the number of data contained in each set '''
+        '''
+        Return the number of data contained in each set
+        
+        Inputs
+        ---------
+        data : str
+            Either train or test
+            
+        Returns
+        ---------
+        Size of the choosen dataset.
+        '''
         
         assert data=='train' or data=='test', f'Data must be train or test, received {data}'
         if data=='train':
@@ -54,7 +76,7 @@ class DataLoader(object):
             return len(self.path_test)
 
     def augment(self, image, mask):
-        ''' Function for augmentation, if neccessary '''
+        ''' Function for augmentation '''
         return self.aug(image=image, segmentation_maps=mask)
         
     def norm(self, img):
@@ -74,35 +96,53 @@ class DataLoader(object):
             
             # open images
             img = imread(path, resize=resize)
-            # open mask based on the image path
-            mask = imread(path.replace('image', 'mask'), resize=resize)[...,0]
-
-            # adjust mask
-            _,mask = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            mask = SegmentationMapsOnImage(np.uint8(mask/255.), shape=img.shape)
-
+            
+            # open all masks based on the image path
+            mask = np.zeros((*img.shape[:2], self.num_classes), dtype='uint8')
+            path_mask = path.replace('image.jpg', 'mask')
+            for pm in glob(path_mask+'*.jpg'):
+                # get the class for the mask
+                clas = int(pm.replace('.jpg', '').split('_')[-1])
+                msk  = imread(pm, resize=resize)[...,0]
+                
+                # adjust mask
+                msk = cv2.threshold(msk,0,1,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+                mask[...,clas] = msk
+            
+            mask = SegmentationMapsOnImage(mask, shape=img.shape)
+            
             # augmentation
             if augment:
                 img, mask = self.augment(img, mask)
 
             mask = mask.get_arr()
             img = self.norm(img)
-
-            # draw contour based on the mask
+            
+            # draw contours
             contours = np.zeros_like(mask)
-            cnts, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            contours = cv2.drawContours(contours, cnts, -1, (1,1,1), 2)
+            for i in range(mask.shape[-1]):
+                base = np.zeros_like(img)
+                cnts = cv2.findContours(mask[...,i], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+                contours[...,i] = cv2.drawContours(base, cnts, -1, (1,1,1), 2)[...,0]
 
-            # adjust channels
-            contours = contours[...,np.newaxis]
-            mask = mask[...,np.newaxis]
-
-            yield np.float32(img), {'mask_output':np.float32(mask), 'contour_output':np.float32(contours)}
+            yield np.float32(img), {'mask_output':np.float32(mask),
+                                    'contour_output':np.float32(contours)}
             
         return generator
             
     def flow(self, data, batch_size, resize=None):
-        ''' Tensorflow iterator '''
+        '''
+        Tensorflow iterator
+        
+        Inputs
+        ----------
+        data : str
+            Dataset to be used. Either train or test.
+        batch_size : int
+            Size of the batch.
+        resize : tuple, default=None
+            Value to resize images.
+        '''
         
         # verify dataset to be used
         if tf.equal(data, 'train'):
@@ -115,13 +155,16 @@ class DataLoader(object):
         # index to each path sample
         indexes = [i for i in range(len(paths))]
         
+        # get one image to verify shapes
+        x, y = next(self.get_generator(paths, augment, resize)(0))
+        
         # Tensorflow Dataset API options
         with tf.device('/cpu:0'):
             dataset = tf.data.Dataset
             dataset = dataset.from_tensor_slices(indexes)
             dataset = dataset.interleave(lambda index:tf.data.Dataset.from_generator(self.get_generator(paths, augment, resize),
                             (tf.float32, {'mask_output':tf.float32, 'contour_output':tf.float32}),
-                            output_shapes=((224,224,3), {'mask_output':(224,224,1), 'contour_output':(224,224,1)}),
+                            output_shapes=(x.shape, {'mask_output':y[0].shape, 'contour_output':y[1].shape}),
                             args=(index,)),
                         cycle_length=batch_size,
                         block_length=1,
@@ -132,43 +175,95 @@ class DataLoader(object):
         return dataset
                     
     def view_data(self, data='train', batch_size=4, resize=None):
-        ''' Method visualize the data returned by the generator (to verification purposes) '''
+        '''
+        Method to visualize the data returned by the generator (for verification purposes)
+        
+        Inputs
+        ----------
+        data : str
+            Dataset to visualized. Either train or test.
+        batch_size : int
+            Size of the batch.
+        resize : tuple, default=None
+            Value to resize images.
+        '''
         
         assert data=='train' or data=='test', f'Data must be train or test, received {data}'
         
         x, y = next(iter(self.flow(data, batch_size, resize)))
-        x = x.numpy(); y1 = y['mask_output'].numpy(); y2 = y['contour_output'].numpy()
+        x = x.numpy(); mask = y['mask_output'].numpy(); cnt = y['contour_output'].numpy()
         
         print('Batch X: ', x.shape, x.min(), x.max())
-        print('Batch Y1: ', y1.shape, y1.min(), y1.max())
-        print('Batch Y2: ', y2.shape, y2.min(), y2.max())
+        print('Batch Mask: ', mask.shape, mask.min(), mask.max())
+        print('Batch Contour: ', cnt.shape, cnt.min(), cnt.max())
+        
+        mask = [SegmentationMapsOnImage(m, shape=img.shape) for m in mask]
+        cnt  = [SegmentationMapsOnImage(c, shape=img.shape) for c in cnt]
         
         plt.figure(figsize=(10,10))
         for i in range(x.shape[0]):
             plt.subplot(x.shape[0]//2+1, 2, i+1)
-            plt.imshow(np.hstack([self.denorm(x[i]),
-                                  cv2.merge([self.denorm(y1[i][...,0])]*3),
-                                  cv2.merge([self.denorm(y2[i][...,0])]*3)]))
+            plt.imshow(np.hstack([
+                                  self.denorm(x[i]),
+                                  mask[i].draw_on_image(self.denorm(x[i])),
+                                  cnt[i].draw_on_image(self.denorm(x[i]))
+                                ]))
             
     def predict_data(self, model, data='test', batch_size=1, resize=None):
-        ''' Method to visualize trained model on train/test data '''
+        '''
+        Method to visualize the output of a trained model on train/test data
+        
+        Inputs
+        ----------
+        model: tf.keras.models.Model
+            Model to be used.
+        data : str
+            Dataset to visualized. Either train or test.
+        batch_size : int
+            Size of the batch.
+        resize : tuple, default=None
+            Value to resize images.
+        '''
         
         assert data=='train' or data=='test', f'Data must be train or test, received {data}'
         
         x, y = next(iter(self.flow(data, batch_size, resize)))
-        x = x.numpy(); y = y['mask_output'].numpy()
-        p = model.predict(x)[0]
+        x = x.numpy(); mask = y['mask_output'].numpy()
+        
+        pred = model.predict(x)[0]
+        
+        mask = [SegmentationMapsOnImage(m, shape=img.shape) for m in mask]
+        pred = [SegmentationMapsOnImage(p, shape=img.shape) for p in pred]
         
         plt.figure(figsize=(10,10))
         for i in range(x.shape[0]):
             plt.subplot(x.shape[0]//2+1, 2, i+1)
-            plt.imshow(np.hstack([self.denorm(x[i]), cv2.merge([self.denorm(y[i][...,0])]*3), cv2.merge([self.denorm(p[i][...,0])]*3)]))
+            plt.imshow(np.hstack([
+                                  self.denorm(x[i]),
+                                  mask[i].draw_on_image(self.denorm(x[i])),
+                                  pred[i].draw_on_image(self.denorm(x[i]))
+                                ]))
             plt.title('Input | Ground truth | Predicted')
     
     def predict_input(self, model, input_image):
-        ''' Method to visualize trained model on an input image '''
+        '''
+        Method to visualize trained model on an input image
         
-        input_image = self.norm(input_image)
-        p = model(input_image[np.newaxis,...])[1].numpy()
+        Inputs
+        ----------
+        model: tf.keras.models.Model
+            Model to be used.
+        data : str
+            Dataset to visualized. Either train or test.
+        batch_size : int
+            Size of the batch.
+        resize : tuple, default=None
+            Value to resize images.
+        '''
         
-        return cv2.merge([self.denorm(p[0,...,0])]*3)
+        input_data = self.norm(input_image)
+        p = model.predict(input_data[np.newaxis,...])[0][0,...]
+        
+        pred = SegmentationMapsOnImage(p, shape=input_image.shape)
+        
+        return p.draw_on_image(input_image)
